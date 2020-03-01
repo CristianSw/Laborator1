6fcabf42f3966d7bebe1dfb6e654260bfac8cd22
CristianSw <cristian.sw@protonmail.com>
2020-03-01 17:19:41 +0200

    finalize 1st part of XML, start working about 2nd part

--- src/main/java/org/example/xml/CreateXML.java
+++ src/main/java/org/example/xml/CreateXML.java
@@ -41,17 +41,49 @@ public class CreateXML {
             Attr attrType = doc.createAttribute("tag2_ID");
             attrType.setValue("001");
             studentName.setAttributeNode(attrType);
+
             Element t1 = doc.createElement("t1");
             t1.appendChild(doc.createTextNode("string"));
             studentName.appendChild(t1);
+
+            Element t2 = doc.createElement("t2");
+            t2.appendChild(doc.createTextNode("string"));
+            studentName.appendChild(t2);
+
+            Element t3 = doc.createElement("t3");
+            t3.appendChild(doc.createTextNode("28.10.1998"));
+            studentName.appendChild(t3);
+
+            Element t4 = doc.createElement("t4");
+            t4.appendChild(doc.createTextNode("string"));
+            studentName.appendChild(t4);
             bestStudent.appendChild(studentName);
 
-            Element studentName2 = doc.createElement("studentName");
-            Attr attrType2 = doc.createAttribute("name");
-            attrType2.setValue("Cristian");
-            studentName2.setAttributeNode(attrType2);
-            studentName2.appendChild(doc.createTextNode("10000000000000000000"));
-            bestStudent.appendChild(studentName2);
+            Element listElements = doc.createElement("listElements");
+            Element listElement = doc.createElement("listElement");
+            listElement.appendChild(doc.createTextNode("string"));
+            listElements.appendChild(listElement);
+            studentName.appendChild(listElements);
+
+            Element listElement2 = doc.createElement("listElement");
+            listElement2.appendChild(doc.createTextNode("string"));
+            listElements.appendChild(listElement2);
+            studentName.appendChild(listElements);
+
+            Element listElement3 = doc.createElement("listElement");
+            listElement3.appendChild(doc.createTextNode("string"));
+            listElements.appendChild(listElement3);
+            studentName.appendChild(listElements);
+
+            Element t5 = doc.createElement("t5");
+            t5.appendChild(doc.createTextNode("123456789"));
+            studentName.appendChild(t5);
+
+
+            attrType.setValue("002");
+            studentName.setAttributeNode(attrType);
+            studentName.appendChild(doc.createTextNode("10000000000000000000"));
+            bestStudent.appendChild(studentName);
 
 //            Write in XML file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
